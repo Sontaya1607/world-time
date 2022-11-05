65f4b4fee6887aab5e907962a8dd698ac7214d42
Sontaya Rungsaard <sontaya16071997@gmail.com>
2022-11-05 17:26:22 +0700

    feat: setup components

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/q
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [
+    vue(),
+    Components(),
+  ],
 })
