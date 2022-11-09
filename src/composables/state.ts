4a64129990c94296dcad50964a70ea101459fc73
Sontaya Rungsaard <sontaya16071997@gmail.com>
2022-11-09 23:42:49 +0700

    feat: timezone actions

--- src/composables/state.ts
+++ src/composables/state.ts
@@ -6,6 +6,21 @@ export const zones = computed(() => zoneNames.value.map(name => timezones.find(i
 export function addToTimezone(timezone: Timezone) {
   zoneNames.value.push(timezone.name)
 }
+
+export function removeZone(timezone: Timezone) {
+  zoneNames.value = zoneNames.value.filter(name => name !== timezone.name)
+}
+
+export function moveZone(timezone: Timezone, delta: 1 | -1) {
+  const index = zoneNames.value.indexOf(timezone.name)
+  if (index === -1)
+    return
+  const target = index + delta
+  const other = zoneNames.value[target]
+  zoneNames.value[target] = timezone.name
+  zoneNames.value[index] = other
+}
+
 const userTimezone = new window.Intl.DateTimeFormat().resolvedOptions().timeZone
 
 if (!zones.value.length)
